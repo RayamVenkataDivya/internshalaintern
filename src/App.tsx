0fbb12ab0de7dd36fff781e5e6af10a1a09446ee
RayamVenkataDivya <rayam162002@gmail.com>
2023-08-20 15:33:11 +0530

    table

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,49 @@
 import './App.css'
 
 function App() {
+  const students=[
+    {
+      "firstname":"divya",
+      "lastname":"chowdary",
+      "gender":"female"
+        
+    },
+    {
+      "firstname":"bharath",
+      "lastname":"reddy",
+      "gender":"male"
+        
+    },
+    {
+      "firstname":"sireesha",
+      "lastname":"reddy",
+      "gender":"female"
+        
+    },
+    {
+      "firstname":"harshit",
+      "lastname":"chowdary",
+      "gender":"male"
+        
+    },
+  ]
   
   return (
     <>
-    <h1>Welcome todo</h1>
+    <table border={2}>
+      <thead>
+        <th>Firstname</th>
+        <th>lastname</th>
+        <th>gender</th>
+      </thead>
+      <tbody>
+        {
+          students.map((a)=>{
+            return <tr style={(a.gender==="female")?{backgroundColor:"pink"}:{backgroundColor:"lightblue"}}><td>{a.firstname}</td><td>{a.lastname}</td><td>{a.gender}</td></tr>
+          })
+        }
+      </tbody>
+    </table>
     </>
   )
 }
